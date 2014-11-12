2a9660d1a9ed111b435099071bf71adb41512548
aa223gg <aa223gg@student.lnu.se>
2014-11-12 15:25:03 +0000

    Vandalen: datatyper och namnhantering fungerar

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -1,8 +1,40 @@
 "use strict";
 
 var makePerson = function(persArr){
-
+    
+    var names = [];
+    var ages = [];
+     
+    
+    names = persArr.map(function(person){return person.name}); 
+    names.sort(function (a,b){return a.localeCompare(b, 'sv');});
+    
+    ages = persArr.map(function(person){return person.ages}); 
+    ages.sort(function (a,b) {return a-b});
+    
+    
+    var maxAge = Math.max.apply(Math, ages);
+    var minAge = Math.min.apply(Math, ages);
+    
+    var averageAge;
+    
+   
+    return {
+        minAge: minAge, 
+        maxAge: maxAge, 
+        averageAge: averageAge, 
+        names: names.join(", ")}; 
+    
+};
+    
+    
+    
+    
+    
+    
+    
+    
     
 
-}
+
 

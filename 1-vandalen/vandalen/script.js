5d9b8a381c5f2c0901c20e47b4b617d4f72a3a31
aa223gg <aa223gg@student.lnu.se>
2014-11-13 19:31:48 +0000

    Vandalen klar

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -4,27 +4,28 @@ var makePerson = function(persArr){
     
     var names = [];
     var ages = [];
-     
-    
-    names = persArr.map(function(person){return person.name}); 
-    names.sort(function (a,b){return a.localeCompare(b, 'sv');});
-    
-    ages = persArr.map(function(person){return person.ages}); 
-    ages.sort(function (a,b) {return a-b});
-    
-    
-    var maxAge = Math.max.apply(Math, ages);
-    var minAge = Math.min.apply(Math, ages);
-    
+    var maxAge;
+    var minAge;
+    var agesSum;
     var averageAge;
     
-   
+    names = persArr.map(function(person){return person.name});      //Hämtar ut namnen från arrayen och sorterar i ny array
+    names.sort(function (a,b){return a.localeCompare(b, 'sv');});   //Sorterar i bokstavsordning
+    
+    ages = persArr.map(function(person){return person.age}); 
+    ages.sort();
+    
+    maxAge = Math.max.apply(Math, ages);
+    minAge = Math.min.apply(Math, ages);
+    
+    agesSum = ages.reduce(function(a, b){return a + b;});
+    averageAge = Math.round(agesSum / ages.length); 
+    
     return {
         minAge: minAge, 
         maxAge: maxAge, 
         averageAge: averageAge, 
         names: names.join(", ")}; 
-    
 };
     
     

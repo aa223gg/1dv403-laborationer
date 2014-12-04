518832aaf01caf48c64626d0b7a0b4933752d38b
aa223gg <aa223gg@student.lnu.se>
2014-12-04 14:20:31 +0000

    Jobbar på att vända tillbaka bilderna

--- 3-gameon/memory/script/memory.js
+++ 3-gameon/memory/script/memory.js
@@ -2,20 +2,24 @@
 
 var Memory = {
     
-    //Skapa en egenskap på ditt objekt som senare kommer att referera till den utslumpade arrayen. (jmfr. messages-arrayen i Laboration 2)
+    
     memoryNumbers: [],
+    pairs: [],              //array för paren
+    numberOfTries: 0,
+    numberOfPairs: 0,
     
     init: function(){
-        //I init-metoden anropar du arrayslumpsmetoden och sparar resultatet i egenskapen du skapade i 4an.
+        
         var rows = 4;
         var cols = 4;
+       
         Memory.memoryNumbers = RandomGenerator.getPictureArray(rows, cols);
         
-        Memory.createTable(rows, cols, Memory.memoryNumbers);
+        Memory.createTable(rows, cols);
     },   
        
-    createTable: function(rows, cols, memoryNumbers){   
-       var k = 0;
+    createTable: function(rows, cols){   
+       var index = 0;                                               //Håller reda på vilken bild i arrayen
        var table = document.createElement("table");                 //Skapa tabell
         
         for (var i = 1; i <= rows; i += 1){
@@ -38,12 +42,57 @@ var Memory = {
                 var boardgame = document.getElementById("boardgame");
                 boardgame.appendChild(table);
                 
-                a.addEventListener("click", Memory.turnPiece);
-                }
+                Memory.turnPiece(index, a, img);
+                index += 1;
+            }
             }
          },
         
-    turnPiece: function(){
+    turnPiece: function(index, a, img){
+        
+        var turnedPiece = "pics/" + Memory.memoryNumbers[index] + ".png";
+               
+                a.onclick = function(){ 
+               
+                    if (img.getAttribute("src") === "pics/0.png"){
+                        Memory.pairs.push(a); 
+                    }
+                    
+                    if (Memory.pairs.length <= 2){
+                       img.setAttribute("src", turnedPiece);
+                    }
+                    
+                    
+               if (Memory.pairs.length === 2){
+                    Memory.numberOfTries += 1;
+                    var result = document.getElementById("result");
+                    result.innerHTML = "Antal försök: " + Memory.numberOfTries;
+                    
+                    if(Memory.pairs[0] === Memory.pairs[1]){    //if(Memory.pairs[0].src === Memory.pairs[1].src){   ??? Funkar ej!
+                      Memory.numberOfPairs += 1;
+                      console.log(Memory.numberOfPairs);
+                    }
+                    
+                    else {
+                        console.log("inget par");
+                        setTimeout(function() {
+                            //img.src = "pics/0.png";   //Vänder endast sista bilden
+                            
+                         Memory.pairs[0].src = "pics/0.png";        //funkar inte alls
+                      Memory.pairs[1].src = "pics/0.png";
+                      Memory.pairs = [];
+                        }, 1000);
+                   }
+                   
+                   
+               
+                    
+              }
+               
+               
+               
+              
+               };
                
     },
 };

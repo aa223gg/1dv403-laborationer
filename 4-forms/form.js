c1952e5714cdbd88e129b448df5fb9d18f56cdb0
aa223gg <aa223gg@student.lnu.se>
2014-12-12 11:49:43 +0000

    Lagt till resultat

--- 4-forms/form.js
+++ 4-forms/form.js
@@ -8,19 +8,24 @@ window.onload = function(){
         getQuestion("http://vhost3.lnu.se:20080/question/1");
     });
         
+    var questionNumber = 0;
+    var numberOfTries = 1;
     
     function getQuestion(questionURL){
         
         var xhr = new XMLHttpRequest();                                         //Hämta frågan
-         
+        
         xhr.onreadystatechange = function() {
+           
            
             if (xhr.readyState === 4){
                 if (xhr.status === 200){
                     var text = JSON.parse(xhr.responseText);
                     document.getElementById("question").innerHTML = text.question;
+                    questionNumber +=1;
+                    document.getElementById("start").innerHTML = "Fråga nr: " + questionNumber;
                     
-                    createResponseArea(text.nextURL);
+                    createResponseArea(text.nextURL, text.id, questionNumber);
                 }
             }
          };
@@ -32,7 +37,7 @@ window.onload = function(){
         
     }
     
-        function createResponseArea(nextURL){
+        function createResponseArea(nextURL, answerID, questionNumber){
             var input = document.createElement("input");                            //Skapa textruta för svar
             input.type = "text";
             var response = document.getElementById("answer");
@@ -50,17 +55,17 @@ window.onload = function(){
             
             sendButton.addEventListener("click", function(e){                        //Oncklick på "Svara"
                 e.preventDefault();
-                sendAnswer(nextURL);
+                sendAnswer(nextURL, answerID, questionNumber);
             });
             
             input.addEventListener("keypress", function(e){                         //Svara med enter
                 if(!e) e = window.event;
                 if (e.keyCode === 13){
-                    sendAnswer(nextURL);
+                    sendAnswer(nextURL, answerID, questionNumber);
                 }
         });
         
-        function sendAnswer(nextURL){
+        function sendAnswer(nextURL, answerID, questionNumber){
             
             var xhrAnswer = new XMLHttpRequest();
          
@@ -69,12 +74,13 @@ window.onload = function(){
                     if (xhrAnswer.readyState === 4){
                         if (xhrAnswer.status === 200){
                             var answerText = JSON.parse(xhrAnswer.responseText);
-                            correctAnswer(answerText.nextURL);
+                            correctAnswer(answerText.nextURL, numberOfTries, questionNumber);
                             
                         }
                         else {
-                            console.log(getQuestion.text);
-                            wrongAnswer();
+                            
+                            numberOfTries += 1;
+                            wrongAnswer(answerID);
                         }
                     }
                 };
@@ -88,10 +94,14 @@ window.onload = function(){
                 
         }
         
-        function correctAnswer(nextQuestionURL){
+        function correctAnswer(nextQuestionURL, numberOfTries, questionNumber){
             document.getElementById("question").innerHTML = "";
             document.getElementById("answer").innerHTML = "";
             document.getElementById("question").innerHTML = "Rätt svar!";
+            var result = document.getElementById("result");
+            var resultPerQuestion = document.createTextNode("Du klarade fråga " + questionNumber + " på " + numberOfTries + " försök.");
+            result.appendChild(resultPerQuestion);
+            numberOfTries = 0;
             
             var nextQuestion = document.createElement("input");                       //Skapa knapp "Nästa fråga"
             nextQuestion.type = "button";
@@ -102,18 +112,39 @@ window.onload = function(){
             
             nextQuestion.addEventListener("click", function(e){                        //Oncklick på "Nästa fråga"
                 e.preventDefault();
-                
+                if (nextQuestionURL === undefined){
+                    gameOver();
+                }
+                else {
+                getQuestion(nextQuestionURL);
+                }
                 
             });
             
         }
             
-        function wrongAnswer(){
+        function wrongAnswer(answerID){
             document.getElementById("question").innerHTML = "";
             document.getElementById("answer").innerHTML = "";
-            document.getElementById("answer").innerHTML = "Fel. Försök igen.";
+            document.getElementById("question").innerHTML = "Fel.";
             
-            //getQuestion(samma URL);
+            var tryAgain = document.createElement("input");                       //Skapa knapp "Försök igen"
+            tryAgain.type = "button";
+            tryAgain.value = "Försök igen";
+            var aNext = document.createElement("a");
+            aNext.appendChild(tryAgain);
+            document.getElementById("answer").appendChild(aNext);
+            
+            tryAgain.addEventListener("click", function(e){                        //Oncklick på "Nästa fråga"
+                e.preventDefault();
+                getQuestion("http://vhost3.lnu.se:20080/question/" + answerID);
+                
+            });
+        }
+        
+        function gameOver() {
+            document.getElementById("question").innerHTML = "Grattis! Du har klarat alla frågor!";
+            document.getElementById("answer").innerHTML = "";
         }
         
         }

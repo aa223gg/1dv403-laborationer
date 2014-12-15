7afd49abcdd7a3ef4f451bcf93af48282b0520cd
aa223gg <aa223gg@student.lnu.se>
2014-12-15 08:29:55 +0000

    Färdigställt och lagt till kommentarer

--- 4-thequiz/quiz.js
+++ 4-thequiz/quiz.js
@@ -24,7 +24,7 @@ var Quiz = {
            
             if (xhr.readyState === 4){
                 if (xhr.status === 200){
-                    var text = JSON.parse(xhr.responseText);
+                    var text = JSON.parse(xhr.responseText);                    //Sträng med texten vi fick från servern
                     document.getElementById("question").innerHTML = text.question;
                     document.getElementById("start").innerHTML = "";                                                             
                     document.getElementById("questionNumber").innerHTML = "Fråga nr: " + Quiz.questionNumber;
@@ -47,7 +47,7 @@ var Quiz = {
             var response = document.getElementById("answer");
             response.innerHTML = "";
             response.appendChild(Quiz.input);
-            Quiz.input.focus();                                               
+            Quiz.input.focus();                                                 
             
             var sendButton = document.createElement("input");                   //Skapa knapp "Svara"
             sendButton.type = "button";
@@ -76,12 +76,12 @@ var Quiz = {
                 xhrAnswer.onreadystatechange = function() {
            
                     if (xhrAnswer.readyState === 4){
-                        if (xhrAnswer.status === 200){
+                        if (xhrAnswer.status === 200){                          //Vid rätt svar anropas correctAnswer och nextURL skickas med
                             var answerText = JSON.parse(xhrAnswer.responseText);
                             Quiz.correctAnswer(answerText.nextURL); 
                         }
                         else {
-                            Quiz.numberOfTries += 1;
+                            Quiz.numberOfTries += 1;                            //Vid fel svar ökas antal försök med ett och wrongAnswer anropas
                             Quiz.wrongAnswer();  
                         }
                     }
@@ -95,15 +95,14 @@ var Quiz = {
     },
     
     correctAnswer: function(nextQuestionURL){  
-        document.getElementById("question").innerHTML = "";                     //Text vid rätt svar
-        document.getElementById("answer").innerHTML = "";
+                     
+        document.getElementById("answer").innerHTML = "";                       //Text vid rätt svar
         document.getElementById("question").innerHTML = "Rätt svar!";
-            
-            
+        
         var resultObject = {tries: Quiz.numberOfTries, number: Quiz.questionNumber};    //Skapa resultatobjekt med antal försök och frågans nummer och lägg i arrayen
         Quiz.questionResult.push(resultObject);
            
-        Quiz.numberOfTries = 1;
+        Quiz.numberOfTries = 1;                                                 //Återställ antal försök till 1 inför nästa fråga
         
         var nextQuestion = document.createElement("input");                     //Skapa knapp "Nästa fråga"
         nextQuestion.type = "button";
@@ -143,11 +142,11 @@ var Quiz = {
     },
     
     gameOver: function() {
-        document.getElementById("start").innerHTML = "";
+        document.getElementById("start").innerHTML = "";                        //Text när man klarat alla frågor
         document.getElementById("question").innerHTML = "Grattis! Du har klarat alla frågor!";
         document.getElementById("answer").innerHTML = "";
             
-        for (var i = 0; i < Quiz.questionNumber; i += 1){
+        for (var i = 0; i < Quiz.questionNumber; i += 1){                       //Resultatet med antal försök per fråga redovisas
             var resultPerQuestion = document.createTextNode("Du klarade fråga " + Quiz.questionResult[i].number + " på " + Quiz.questionResult[i].tries + " försök.");
             var result = document.getElementById("result");
             var br = document.createElement("br");
